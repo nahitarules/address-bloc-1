2d4531dfdc16d8fca4c1e86145a274554f043357
nahome <nahom.teshome@yahoo.com>
2018-07-19 14:45:24 -0700

    order

--- models/address_book.rb
+++ models/address_book.rb
@@ -3,7 +3,6 @@ require "csv"
 require 'bloc_record/base'
 
 class AddressBook < BlocRecord::Base
-  attr_reader :entries
 
   def initialize(options={})
     super
@@ -14,6 +13,14 @@ class AddressBook < BlocRecord::Base
     Entry.create(name: name, phone_number: phone, email: email, address_book_id: self.id)
   end
 
+  def entries
+     Entry.where(address_book_id: self.id)
+   end
+
+   def find_entry(name)
+     Entry.where(name: name, address_book_id: self.id).first
+   end
+
   def import_from_csv(file_name)
     # Implementation goes here
     csv_text = File.read(file_name)
@@ -22,5 +29,5 @@ class AddressBook < BlocRecord::Base
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
-  end  
+  end
 end
